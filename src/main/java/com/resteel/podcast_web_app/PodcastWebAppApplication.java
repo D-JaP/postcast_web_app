ed2225eb353931ff433f864799feb40e4da62f95
dzung <dzung.leanh218@gmail.com>
2023-06-24 23:58:40 +1000

    project setup

---
+++ src/main/java/com/resteel/podcast_web_app/PodcastWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.resteel.podcast_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastWebAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resteel/podcast_web_app/PodcastWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resteel.podcast_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
